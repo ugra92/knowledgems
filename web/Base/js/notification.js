d25cf2fffb28ff31ff3e890d04d62de2ea3c29f6
ugra92 <ugra1992@gmail.com>
2015-09-10 21:32:25 +0200

    Notification system bugfix

--- web/Base/js/notification.js
+++ web/Base/js/notification.js
@@ -2,8 +2,16 @@
 $('.notification-li').on('click', function(e){
     e.preventDefault();
     var id = $(this).data('id');
-    $.post('/notification/see', {id:id}, function(data) {
-        console.log(data);
-        window.location.href = "article/" + id;
+    var notificationID = $(this).data('notificationid');
+    var type = $(this).data('type');
+    $.post('/notification/see', {id:notificationID}, function(data) {
+       switch (type){
+           case "video":{
+               window.location.href = window.location.origin+"/video/" + id;
+           }return;
+           case "article":{
+               window.location.href = window.location.origin+"/article/" + id;
+           }return;
+       }
     });
 });
